5d4fac58c855584c6894b53a8fa1b6911e6d87ba
eedeidk <59429783+eedeidk@users.noreply.github.com>
2023-01-30 18:04:01 +0530

    main parser v0.1

--- utils/parser.py
+++ utils/parser.py
@@ -1,27 +1,82 @@
 from bs4 import BeautifulSoup as bs
-import requests, time
+import requests, time, re
+import pandas as pd
 
-urls = ['https://www.wbhealth.gov.in/pages/notice', 'https://www.wbhealth.gov.in/pages/notice', 'https://www.wbhealth.gov.in/pages/career']
+class Parser:
+	''''Parser object and functions'''
+	def __init__(self, urls_list):
+		'''Creates session with a user-agent'''
+		self.sess = requests.Session()
+		UA = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; rv:106.0) Gecko/20100101 Firefox/106.0'}
+		self.sess.headers.update(UA)
+		self.urls_list = urls_list
 
-def create_session():
-	'''Creaets session with a user-agent'''
-	sess = requests.Session()
-	UA = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; rv:106.0) Gecko/20100101 Firefox/106.0'}
-	sess.headers.update(UA)
-	return sess
-
-def get_page(url, sess):
-	'''Fetches the url page using session'''
-	response = sess.get(url)
-	i=0
-	if response.status_code == 200:
-		return response
-	else:
-		i=i+1
-		time.sleep(4)
-		if not i>3:
-			return get_page(url, sess)
+	def get_page(self, url):
+		'''Fetches the url page using session'''
+		self.response = self.sess.get(url)
+		i=0
+		if self.response.status_code == 200:
+			return self.response
 		else:
-			return None
+			i=i+1
+			time.sleep(4)
+			if not i>3:
+				return self.get_page(self, url)
+			else:
+				return None
+
+	def page_souper(self):
+		'''Soups the page response to get the table'''		
+		self.soup = bs(self.response.text, 'lxml')
+		tab_cont = self.soup.find('div', class_='middle_content')
+		table_first = tab_cont.find('table')
+		# first table is for searching
+		table_main = table_first.find_next('table')
+		self.table = table_main
+		# with open('test.html', 'w') as html:
+		# 	html.write(str(table_main))
+
+	def table_parse(self):
+		'''Parses the table for latest insertions'''
+		# print(self.table)
+		df = pd.read_html(str(self.table), header=0, flavor=["lxml", "bs4"])[0]
+		# df drop none values
+		df1 = df.dropna(axis=0, how='all')
+		df1 = df1.dropna(axis=1, how='all')
+		# drop 
+		df1 = df1.reset_index(drop=True)
+
+		#do some url append:
+		links = []
+		rows = self.table.find_all("tr")
+		print(len(rows))
+		for row in rows:
+			innr_row = row.find("td")
+			link_c = innr_row.find('a')
+			if not link_c ==None:
+				link = link_c.get('href')
+				# print(link)
+				links.append(link)
+			else:
+				pass
 		
 
+		#check
+		df1['Links'] = links
+		# print(df1.columns)
+		# print(df1.iloc[2].values)
+
+		# save to a dfobject
+		self.df = df1
+		
+
+
+	def main(self):
+		'''Serialize them return dataframe'''
+		for each in self.urls_list:
+			# self.url=each
+			resp = self.get_page(each)
+			if not resp==None:
+				self.page_souper()
+				self.table_parse()
+				return self.df
