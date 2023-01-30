f199632ea49d1e66d2355690d3f91ac8c54b76a5
eedeidk <59429783+eedeidk@users.noreply.github.com>
2023-01-30 22:52:29 +0530

    prettify

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-import os
-urls = ['https://www.wbhealth.gov.in/pages/notice', 'https://www.wbhealth.gov.in/pages/notice', 'https://www.wbhealth.gov.in/pages/career']
-url1 = ['https://www.wbhealth.gov.in/pages/notice']
+import os, asyncio
+urls = ['https://www.wbhealth.gov.in/pages/notice', 'https://www.wbhealth.gov.in/pages/career']
 
-from utils import parser
-df_main = parser.Parser(url1).main()
+url_dict ={
+	'notice': 'https://www.wbhealth.gov.in/pages/notice',
+	'go':'https://www.wbhealth.gov.in/pages/gov_order'
+	}
 
-##extra parse
-df_processed = parser.OptionalParser(0, df_main).main()
-# print(df_processed)
+from utils import parser, sender
+
+# iteration
+for k in url_dict.keys():
+	url = url_dict[k]
+	df_main = parser.Parser(url).main()
+	##extra parse
+	df_processed = parser.OptionalParser(k, df_main).main()
+	# print(df_processed)
+	if not df_processed.empty:
+		print(df_processed.shape[0])
+		TgSend = sender.tgsend(df_processed, df_type=k)
+		asyncio.run(TgSend.main())
+	else:
+		print(k, 'No updates today')
