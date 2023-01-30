bcf37742ac4b070da0bd9a810043d8d64bfcb7b7
eedeidk <59429783+eedeidk@users.noreply.github.com>
2023-01-30 19:45:23 +0530

    sup for v0.2

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ url1 = ['https://www.wbhealth.gov.in/pages/notice']
 
 from utils import parser
 df_main = parser.Parser(url1).main()
+
+##extra parse
+df_processed = parser.OptionalParser(0, df_main).main()
+# print(df_processed)
